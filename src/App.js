bbdccbdc4986870eec7fbdae51785312936c140c
MilonMridha <milonmridha570@gmail.com>
2022-03-17 23:54:01 -0700

    my-react-userpost-project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadPosts></LoadPosts>
     </div>
   );
 }
 
+function LoadPosts (){
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => setPosts(data))
+  },[])
+
+  return(
+    <div>
+      <h2>My All Post : {posts.length}</h2>
+     <div className='all-post'>  {
+        posts.map(post => <SinglePost tittle={post.title} userid={post.userId} body={post.body}></SinglePost>)
+      } </div>
+    </div>
+  )
+}
+function SinglePost (props){
+  return (
+    <div className='single-post'>
+      <h3>Title : {props.tittle}</h3>
+      <p>User Id :{props.userid}</p>
+      <p>Body : {props.body}</p>
+    </div>
+  )
+}
 export default App;
